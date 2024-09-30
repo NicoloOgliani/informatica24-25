ab6e590404f473903b099b36e53b149a47ff13dd
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-09-30 17:32:55 +0000

    esercizio1compiti

--- 1_01_10.c
+++ 1_01_10.c
@@ -1,8 +1,25 @@
 //verificare se un numero dato in input è divisibile sia per 3 sia per 5
 #include <stdio.h>
 int main (){
-    float a;
+    float A;
     printf("inserisci un valore: ");
-    scanf("%f", &a);
-    
+    scanf("%f", &A);
+    if(A %3==0){
+        printf("%f è divisibile per 3", A);
+    }
+    else{
+        printf("%d non è divisibile per 3", A);
+    }
+    if(A %5==0){
+        printf("%f è divisibile per 5", A);
+    }
+    else{
+        printf("%f non è divisibile per 5", A)
+    }
+    if(A % 3==0, A % 5 ==0){
+        printf("%f è divisibile sia per 3 che per 5", A)
+    }
+    else{
+        printf("%f non è divisibile ne per 3 ne per 5", A)
+    }
 }
