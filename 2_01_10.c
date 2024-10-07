ca968012cb2b8b69de641d35898c2d16a7e252f8
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-10-07 10:55:12 +0000

    es1ottobre.2

--- 2_01_10.c
+++ 2_01_10.c
@@ -1,7 +1,7 @@
 //dati tra numeri stampare: il più piccolo, il più grande e la loro media aritmetica
 #include <stdio.h>
 int main (){
-    float a, b, c, media;
+    float a, b, c, max, min, media;
     printf("inserisci il valore di a: ");
     scanf("%f", &a);
     printf("inserisci il valore di b: ");
@@ -9,21 +9,27 @@ int main (){
     printf("inserisci il valore di c: ");
     scanf("%f", &c);
     media=(a+b+c)/3;
-    if(a>b>c){
-        printf("%f è il numero maggiore", a)
-        pritf("%f è il numero minore", c)
+    max=1;
+    min=1;
+    if(max<b){
+        max=b;
+        if(max<c);
+        max=c;
     }
     else{
-        if(b>c>a){
-            printf("%f è il numero maggiore",b)
-            printf("%f è il numero minore", a)
-        }
-        else{
-            printf("%f è il numero maggiore", c)
-            printf("%f è il numero minore, b")
-            }
-            
-        }
+        if(max<c);
+        max=c;
     }
-    printf("la media dei 3 valri è: %f", media)
+    printf("il massimo è %f", max);
+    if(min<b){
+        min=b;
+        if(min>c);
+        min=c;
+    }
+    else{
+        if(min>c)
+            min=c;
+    }
+    printf("il minimo è %f", min);
+    printf("la media è %f", media);
 }
