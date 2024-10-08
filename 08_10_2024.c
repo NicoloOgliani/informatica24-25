c555941cfc45aa7f8e1016df716e63d37c1bfb2a
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-10-08 11:56:45 +0000

    anno_bisestile_mese_giorno

--- 08_10_2024.c
+++ 08_10_2024.c
@@ -6,6 +6,7 @@ int main(){
     int giorno=0;
     int mese=0;
     int anno=0;
+    int bisestile=0;
 
     printf("inserisci il giorno");
     scanf("%d", &giorno);
@@ -17,16 +18,47 @@ int main(){
     multiplo di 400*/
     if(anno%100==0){
         if(anno%400==0){
-            printf("l'anno è bisestile");M
+            printf("l'anno è bisestile ");
+            bisestile=1;
         }
     }
     else{
         if(anno%4==0){
             printf("l'anno è bisestile");
-        }
-        else{
-            
+            bisestile=1;
         }
     }
+    //CONTROLLO MESE e giorno
+    if(mese>0&&mese<=12){
+        if(mese==2){
+            if(giorno>=1 && giorno<=28+bisestile);{
+            //la variabile bisestile va a uno solo se l'anno è bisestile (verificato prima)
+            printf("la data è accettabile");}
+            else{
+                printf("la data è accettabile ");
+            }
+        }
+        else{
+            if(mese==11|| mese==4 || mese==6|| mese==9){
+                if(giorno>=1 && giorno <=30){
+                    printf("la data è accettabile ");
+
+                }
+                else{
+                    printf("la data non è accettabile");
+                }
+            }
+            else{
+                if(giorno>=1&&giorno<=31){
+                    printf(" la data è accettabile ");
+                }
+                else{
+                    printf("la data non è accettabile");
+                }
+            }
+        }
+
+    }
+    return 0;
 
 }
