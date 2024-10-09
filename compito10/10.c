73872e46a7abec846a8084db4ea0f15a6d9af85b
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-10-09 19:17:19 +0000

    10_10.c

--- compito10/10.c
+++ compito10/10.c
@@ -1,9 +1,20 @@
+/*Scrivi un programma che permetta all’utente di inserire 5 numeri interi
+ e verifichi se questi numeri appartengono alla sequenza dei numeri naturali,
+  cioè sono tutti differenti e hanno tra di loro distanza unitaria
+ nella linea dei numeri. (ad esempio la sequenza 3-4-5-6-7 è una sequenza adeguata,
+  3-4-5-7-8 non è una sequenza adeguata)*/
 #include <stdio.h>
 int main(){
     int n1, n2, n3, n4, n5;
     printf("inserisci i 5 numeri");
     scanf("%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5);
-    if(n2==n1+1&& n)
+    if(n2==n1+1&&n3==n2+1&&n4==n3+1&&n5==n4+1){
+        printf("la sequenza è adeguata");
+    }
+    else{
+        printf("la sequenza non è adeguata")
+    }
+
 
 
 }
