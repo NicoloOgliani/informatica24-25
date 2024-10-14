7d6dfb0a495f908472ac86a71f5518139b892840
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-10-14 10:52:24 +0000

    14_10percasa

--- 14_10galotta.c
+++ 14_10galotta.c
@@ -1,8 +1,8 @@
 /*DATE DUE DATE ESPRESSE IN g, m, a DETERMINARE LA DATA PIU' RECENTE. 
 Esempio:  tra la data 5/07/2024 e  14/07/2024  la più recente è il 14/07/2024.*/
-#include stdio.h
+#include <stdio.h>
 int main(){
-    int giorno1, mese1, anno1, giorno2, mese2, anno2;
+    int anno, bisestile, giorno, mese, giorno1, mese1, anno1, giorno2, mese2, anno2;
      printf("inserisci giorno1: ");
     scanf("%d", &giorno1);
     printf("inserisci mese: ");
@@ -15,6 +15,51 @@ int main(){
     scanf("%d", &mese2);
     printf("inserisci anno2 non maggiore a 2024 ");
     scanf("%d", &anno2);
+    
+    /*un anno è bisestile se è multiplo di 4 ma non di 100 oppure
+    multiplo di 400*/
+    if(anno%100==0){
+        if(anno%400==0){
+            printf("l'anno è bisestile ");
+            bisestile=1;
+        }
+    }
+    else{
+        if(anno%4==0){
+            printf("l'anno è bisestile");
+            bisestile=1;
+        }
+    }
+    //CONTROLLO MESE e giorno
+    if(mese>0 && mese<=12){
+        if(mese==2){
+            if(giorno>=1 && giorno<=28+bisestile)
+                //la variabile bisestile va a uno solo se l'anno è bisestile (verificato prima)
+                printf("la data non è accettabile");
+            else
+                printf("la data è accettabile ");
+            
+        }
+        else{
+            if(mese==11|| mese==4 || mese==6|| mese==9){
+                if(giorno>=1 && giorno <=30){
+                    printf("la data è accettabile ");
+
+                }
+                else{
+                    printf("la data non è accettabile");
+                }
+            }
+            else{
+                if(giorno>=1&&giorno<=31){
+                    printf(" la data è accettabile ");
+                }
+                else{
+                    printf("la data non è accettabile");
+                }
+            }
+        }
+    }
     if(anno1>anno2){
         printf("data1 è più recente");
     }
@@ -23,12 +68,12 @@ int main(){
             printf("la data 1 è la più recente");
         }
         else if(giorno1>giorno2){
-            printf("la data1 è più recente")
+            printf("la data1 è più recente");
         }
 
     }
     else{
-        printf("la data2 è la più recente")
+        printf("la data2 è la più recente");
     }
 
 
