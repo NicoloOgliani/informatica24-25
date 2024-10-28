c920c438f7b23ceda62a618ec926a9a4093746bf
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-10-28 10:50:14 +0000

    menu28_10_02.c

--- 28_10_02.c
+++ 28_10_02.c
@@ -9,19 +9,21 @@
         3.  MOLTIPLICAZIONE
         4.  DIVISIONE  "*/
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 int main(){
-    float num1, num2, scelta;
+    float num1, num2;
+    int scelta;
     printf("inserisci un numero dal menu");
-    printf("0. termina");
-    printf("1. somma");
-    printf("2. differenza");
-    printf("3. moltiplicazione");
-    printf("4. divisione");
+    printf("\n 0. termina");
+    printf("\n 1. somma");
+    printf("\n 2. differenza");
+    printf("\n 3. moltiplicazione");
+    printf("\n 4. divisione");
 
-    printf("scegli l'operazione");
-    scanf("%d", &scelta);
-    srand(time(NULL));//generare numero casuale
+    printf("\n scegli l'operazione");
+    scanf("%f", &scelta);
+    srand(time (NULL));//generare numero casuale
     num1=rand()%50+1;
     num2=rand()%50+1;
 
@@ -48,33 +50,4 @@ int main(){
     return 0;
 }
 
-
-
-
-
-
-    /*
-    if(num<0||num>4){
-        printf(" il numero inserito non va bene");
-    }
-    else if(num==0)
-        printf("termina il programma");
-    else if(num==1)
-        somma1=num1+num2;
-        printf("la somma tra i due numeri è: %f", somma1);
-    else if(num==2)
-        diff2=num1-num2;
-        printf("la diff è uguale a: %f", diff2);
-    else if(num==3)
-        molt3=num1*num2;
-        printf("la molt tra i numeri è uguale a: %f", molt3);
-    else if(num==4)
-        divis4=num1/num2;
-        printf("la divisione tra i due numeri è uguale a: %f",divis4);
-    */
-
-
-
-    
-}    
 
