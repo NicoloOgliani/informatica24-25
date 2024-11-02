fb1ba3a8a9bae7c2b74da28089dc286249abb6b0
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-11-02 13:05:06 +0000

    MorraCinese.c

--- 2_11_01.c
+++ 2_11_01.c
@@ -6,9 +6,10 @@ Data una somma di denaro calcolare il controvalore.*/
 #include <stdio.h>
 int main(){
     float valore, controvalore, dollari, euro;
-    printf("inserisci un valore di denaro in dollari o in euro");
+    printf("dollari_euro=1 \n  euro_dollari=2")
+    printf("scegli un valore dal menu");
     scanf("%f", &valore);
-    if(valore==dollari){
+    if(valore==1){
         controvalore=(valore/1.23);
         printf("il controvalore è: %f", controvalore);
     }
---
+++ 2_11_02.c
@@ -0,0 +1,45 @@
+/*Gioco della Morra cinese: supponendo sasso=1, carta=2, forbice=3, chiedere
+all’utente la sua scelta e far generare al computer la sua.
+Stabilire il vincitore.
+N.B.:per chi non conoscesse il gioco: sasso vince forbice, sasso perde carta, carta perde forbice.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int sasso, carta, forbice, scelta, sceltapc;
+    prinf("sasso=1 \n carta=2 \n  forbice=3");
+    printf("inserisci un valore dal menu");
+    scanf("%d", &scelta);
+    srand(time(NULL));
+    sceltapc = rand() % 3 + 1;
+    if(scelta==1 && sceltapc==1){
+        printf("pareggio");
+
+    }
+    else if(scelta==1&&sceltapc==2){
+        printf("vince il pc");
+
+    }
+    else if(scelta==1&&sceltapc==3){
+        printf("hai vinto tu")
+    }
+    else if(scelta==2&&sceltapc==1){
+        printf("hai vinto tu");
+    }
+    else if(scelta==2&&sceltapc==2){
+        printf("pareggio")
+    }
+    else if(scelta==2&&sceltapc==3){
+        printf("vince il pc");
+    }
+    else if(scelta==3&&sceltapc==1){
+        printf("VINCE IL PC");
+    }
+    else if(scelta==3&&sceltapc==2){
+        printf("HAI VINTO TU")
+    }
+    else if(scelta==3&&sceltapc==3){
+        printf("PAREGGIO");
+    }
+    }
+}
