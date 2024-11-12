7c90628290cc905f57eafc63dbbedda472d4b2d7
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-11-12 20:50:04 +0000

    13_11

--- 13_11.c
+++ 13_11.c
@@ -3,22 +3,19 @@ e che indica quanti valori della sequenza di fibonacci mostrare
 per esempio se l'utente inserisce 6 dovranno essere mostrati 1 1 2 3 5 8 */
 #include <stdio.h>
 int main{
-    int num=0, numval=0;
+    int num=0, numval=0, n1=0, n2=0, n3=0, contatore=0;
     printf("inserisci un numero\n");
     scanf("%d", &num);
     while(num<3){
         printf("inserisci un numero");
         scanf("%d", &numval);
-        while(numval<0 || numval>6){
-            if(numval=0){
-                printf("non ci sono cifre");
-            }
-            else if(numval=1){
-                printf("1\n");
-            }
-            else if(numval=2){
-                printf(" 1 1\n");
-                
+        while(contatore != num-1){
+            n3=n2+n1;
+            n1=n2;//nella n2 il valore più grande e nella n1 il più piccolo
+            n2=n3;//nella n3 cè la somma di n1 e n2
+            contatore=contatore+1;
+            printf("%d", n3\n);
+        }
             }
         }
     }
