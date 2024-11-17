c3c415cbe040d49f5385c315f372a4d68cad0555
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-11-17 10:39:48 +0000

    es1dicara_17_11_2024

--- iterazioni/es1dicara_17_11_2024.c
+++ iterazioni/es1dicara_17_11_2024.c
@@ -3,13 +3,18 @@ strettamente positivo e visualizza tutti i numeri
 pari inferiori a tale numero fino a 0*/
 #include <stdio.h>
 int main(){
-    int num, cnt=0;
+    int num, cnt=0, pari;
+    do{
     printf("Inserisci un numero: ");
     scanf("%d", &num);
-    if(num>=0&& (num%2)==0){
-        do{
-            
-        }
     }
-    printf("I numeri pari inferiori a %d sono: %d ", num, cnt);
+    while(num<0);
+    cnt=num-1;
+    while(cnt>0){
+        pari=cnt%2;
+        if(pari==0){
+            printf("%d", cnt);
+        }
+        cnt=cnt-1;
+    }
 }
