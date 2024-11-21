07b9dce78c862cbf7a14696c9590fba60f42f945
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-11-21 10:13:26 +0000

    ES2OGLIANI.c

--- ES2OGLIANI.c
+++ ES2OGLIANI.c
@@ -2,14 +2,30 @@
 1 e 9) STAMPARE TUTTI I MULTIPLI DI C MINORI DI N.*/
 #include <stdio.h>
 int main(){
-    int n1, nC;
+    int n1, nC, mult1, mult2, mult3, num;
     printf("Inserisci il primo numero");
-    scanf("%d", n1);
-    if(n1>0){
+    scanf("%d", &n1);
+    if(n1>0&&n1<=20){
         printf("Inserisci il valore C");
-        scanf("%d", nC);
+        scanf("%d", &nC);
         if(nC>0 && nC<=9){
-            if(nC)
+            if((2*nC)<=20){
+                mult1=2*nC;
+                
+                if((3*nC)<=20){
+                    mult2=3*nC;
+                    
+          
+
+                }
+                    if((4*nC)<=20){
+                        mult3=4*nC;
+                        printf("%d è multiplo di c e minore di n, %d è multilpo di c e minore di n, %d è multiplo di c e minore di n", mult1, mult2, mult3);
         }
+        }
+    }
+    else{
+        printf("Hai inserito un valore non valido");
+    }
     }
 }
