d7d837439e35ab334ab29eb909ff264fdea0d05c
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-12-01 14:13:12 +0000

    es1for

--- es1for.c
+++ es1for.c
@@ -3,29 +3,19 @@ il metodo delle somme successive dopo aver controllato l’input e accettato sol
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
-
-    // Richiesta dell'input e controllo
+    int num1, num2, prodotto=0;
     do {
         printf("Inserisci il primo numero (maggiore di 0): ");
         scanf("%d", &num1);
-    } while (num1 <= 0);  // Ripete fino a quando il numero non è maggiore di 0
+    } while (num1 <= 0);
 
     do {
         printf("Inserisci il secondo numero (maggiore di 0): ");
         scanf("%d", &num2);
-    } while (num2 <= 0);  // Ripete fino a quando il numero non è maggiore di 0
-
-    // Calcolo del prodotto tramite somme successive
-    int prodotto = 0;
-
-    // Sommiamo 'num1' per 'num2' volte
+    } while (num2 <= 0);
     for (int i = 1; i <= num2; i++) {
-        prodotto += num1;  // Aggiungiamo num1 alla somma
+        prodotto += num1;
     }
-
-    // Stampa del risultato
     printf("Il prodotto di %d e %d è: %d\n", num1, num2, prodotto);
 
-    return 0;
 }
