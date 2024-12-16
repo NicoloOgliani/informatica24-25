36a467b6093024c2b8041a1cb9d49ad8c9a00575
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2024-12-16 16:15:23 +0000

    es1_ripasso

--- iterazioni/es1_ripasso.c
+++ iterazioni/es1_ripasso.c
@@ -1,1 +1,21 @@
 /*stampa i primi n numeri dispari successivi al numero a*/
+#include <stdio.h>
+int main(){
+    int n, a, b;
+    do{
+        printf("Inserisci il valore di a");
+        scanf("%d", &a);
+        printf("Inserisci il numero di valori n: ");
+        scanf("%d", &n);
+    }while(a<0 || n<0);
+    if(a%2==0){
+        a=a+1;
+        printf("%d", a);
+        
+    }
+    b=a;
+    for(int i=1; i<n; i++){
+        b+=2;
+        printf("%d", b);
+    }
+}
