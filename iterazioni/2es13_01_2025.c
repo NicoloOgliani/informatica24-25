71c9fba4b90c976c5a9558b7c17157f8f4f53a30
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-01-13 10:32:15 +0000

    sommaQuadrati

--- iterazioni/2es13_01_2025.c
+++ iterazioni/2es13_01_2025.c
@@ -2,10 +2,16 @@
  dei primi 12 numeri naturali successivi a 8*/
 #include <stdio.h>
 int main(){
-    int i, somma = 0;
-    for (i = 9; i < 21; i++){
+    int inizio, quantita, somma = 0;
+    printf("inserisci il valore iniziale: ");
+    scanf("%d", &inizio);
+    do{
+    printf("inserisci la quantità di numeri: ");
+    scanf("%d", &quantita);
+    }while(quantita <= 0);
+    for (int i = inizio+1; i < inizio+quantita; i++){
         somma += i * i;
     }
-    printf("La somma dei quadrati dei primi 12 numeri naturali successivi a 8 è: %d\n", somma);
+    printf("La somma dei quadrati dei 12 numeri successivi al numero inserito è: %d\n", somma);
     return 0;
 }
