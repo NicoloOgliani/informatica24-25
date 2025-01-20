5c817f7376e2ab0419fb77a8721305117f963873
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-01-20 10:49:23 +0000

    corr_verifica

--- iterazioni/Ogliani_4_16_01_2025.c
+++ iterazioni/Ogliani_4_16_01_2025.c
@@ -5,23 +5,36 @@ valori che rendono la serie crescente o decrescente e restituire quanti numeri e
 Esempio: scelta=crescente - l’utente inserisce: 4 10 3 8 16 - numeri inseriti: 5.*/
 #include <stdio.h>
 int main(){
-    int sceltaordine, num, precedente, cnt = 0;
+    int sceltaordine, num, min_max, cnt = 0;
     printf("Inserisci 1 per stampare i valori in ordine crescente, 2 per stampare i valori in ordine decrescente: ");
     scanf("%d", &sceltaordine);
     printf("Inserisci il numero: ");
     scanf("%d", &num);
-    precedente = num;
+    min_max = num;
     cnt++;
     while(num != 0){
-        printf("Inserisci un numero: ");
-        scanf("%d", &num);
-        if(sceltaordine == 1 && num > precedente){
-            cnt++;
-        }else if(sceltaordine == 2 && num < precedente){
-            cnt++;
+        cnt++;
+        switch(sceltaordine){
+            case 1:{
+                if(num>=min_max){
+                    min_max=num;
+                    printf("%d\n", min_max);
+                }
+                break;
+            }
+            case 2:{
+                if(num<=min_max){
+                    min_max=num;
+                    printf("%d\n", min_max);
+                }
+                break;
+            }
+            default: {
+                printf("Non hai fatto la scelta giusta");
+            }
         }
-        precedente = num;
+        printf("Inserisci un nuovo valore: ");
+        scanf("%d", &num);
     }
-    printf("Numeri inseriti: %d", cnt);
-    return 0;
+    printf("Sono stati inseriti %d valori", cnt);
 }
