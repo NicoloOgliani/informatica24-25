a2825f8d57a27f03be4e89a594ce096082779d7b
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-02-03 10:52:50 +0000

    es1funzioni

---
+++ iterazioni/funzioni/es1funzioni.c
@@ -0,0 +1,26 @@
+/*dato un valore convertirlo in binario*/
+#include <stdio.h>
+#include <math.h>
+
+long converti(int _n);
+
+int main(){
+    int n;
+    long conv=0;
+    do{
+    printf("Inserisci un numero: ");
+    scanf("%d", &n);
+    }while(n<0);
+    conv=converti(n);
+}
+long converti(int _n){
+    int quoz=_n, resto=0, cnt=0, _conv=0;
+
+    while(quoz=!0){
+        resto=quoz%2;
+        quoz=quoz/2;
+        _conv=pow(10, cnt);
+        cnt++;
+    }
+    return _conv;
+}
