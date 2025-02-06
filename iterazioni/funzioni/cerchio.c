e3d95bf915ac02f11ce6d733aacbbe3c6916faf3
NicoloOgliani <nicolo.ogliani@galileiostiglia.edu.it>
2025-02-06 10:56:31 +0000

    cerchio

--- iterazioni/funzioni/cerchio.c
+++ iterazioni/funzioni/cerchio.c
@@ -3,18 +3,18 @@
 
 void calcolatore(float *_r, float *_area, float *_circ);
 int main(){
-    int r, a, c;
+    float r, a, c;
     do{
         printf("Inserisci il raggio: ");
-        scanf("%d", &r);
+        scanf("%f", &r);
     }while(r<=0);
     calcolatore(&r, &a, &c);
-    printf("%d, %d, %d", r, a, c);
+    printf("%f, %f, %f", r, a, c);
 
 
 
 }
 void calcolatore(float *_r, float *_areaa, float *_circ){
-    return _r*_r*3,14;
-    return _r*2*3.14;
+    return *_r**_r*3,14;
+    return *_r*2*3.14;
 }
